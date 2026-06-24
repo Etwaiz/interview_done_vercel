dbc4f97ed744f5e299abb3267750e2ddc7fa41c8
Daniil Oleinik <daniiloleinik000@gmail.com>
2026-06-24 14:05:17 +0300

    xd

--- src/js/app/views/LogsView.jsx
+++ src/js/app/views/LogsView.jsx
@@ -15,7 +15,7 @@ const LogsView = ({ logs}) => {
           <tr className="text-left text-content">
             <th className="w-1/4 border-b border-slate-200 py-2 pr-4 font-medium">{t("textLog3")}</th>
             <th className="w-1/4 border-b border-slate-200 py-2 pr-4 font-medium">{t("textLog10")}</th>
-            <th className="w-1/4 border-b border-slate-200 py-2 pr-4 font-medium">{t("textLog2")}</th>
+            <th className="w-1/5 border-b border-slate-200 py-2 pr-4 font-medium">{t("textLog2")}</th>
             <th className="w-1/4 border-b border-slate-200 py-2 font-medium">{t("textLog4")}</th>
           </tr>
         </thead>
@@ -25,7 +25,7 @@ const LogsView = ({ logs}) => {
               <td className="border-b border-slate-100 py-2 pr-4 whitespace-nowrap">
                 {formatDate(log.timestamp)}
               </td>
-              <td className="border-b border-slate-100 py-2 pr-4">{log.requestId}</td>
+              <td className="border-b border-slate-100 py-2 pr-6">{log.requestId}</td>
               <td className="border-b border-slate-100 py-2 pr-4">
                 {log.role === "user" ? t("text5") : t("text6")}
               </td>
